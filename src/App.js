ec7502e27f0701648d652dbb5c891f3c312d0959
maxcramer <maximiliancramer@yahoo.com>
2022-07-27 16:38:54 +0100

    have stripped app back so only uploading image and recieving from firebase Storage, to look into how to add text to this

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import { useState } from "react";
-import { storage } from "./firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import { useState, useEffect } from "react";
+import { storage, Firestore } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import { v4 } from "uuid";
 
 import "./App.css";
 
 function App() {
+  const [imageList, setImageList] = useState([]);
   const [imageUpload, setImageUpload] = useState(null);
+
+  const imageListRef = ref(storage, "images/");
+  // Calling images from firebase Storage
+  useEffect(() => {
+    listAll(imageListRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  // uploading image to firebase Storage
   const uploadImage = () => {
     if (imageUpload == null) return;
     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then(() => {
-      alert("Image Uploaded");
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
     });
   };
 
   return (
     <div className="App">
-      <h3>Upload File</h3>
-      <input
-        type="file"
-        onChange={(e) => {
-          setImageUpload(e.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload NFT</button>
+      <div>
+        <h3>Upload File</h3>
+        <input
+          type="file"
+          onChange={(e) => {
+            setImageUpload(e.target.files[0]);
+          }}
+        />
+        <button onClick={uploadImage}>Upload NFT</button>
+        {imageList.map((url, index) => {
+          return (
+            <div key={index}>
+              <img src={url} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
